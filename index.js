701afbfc9eed33f549c729b81c9d3a1c28b380ae
Cameron Steele <cam.steeleis@gmail.com>
2019-05-29 14:01:28 -0500

    trying out java on heroku

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 let express = require('express')
-
+var spawn = require('child_process').spawn
 let app = express()
 
 app.get("/", (req, res) => {
+    let jV = spawn("java -v")
+    jv.stdout.on('data', function (data) {
+        console.log('stdout: ' + data.toString());
+      });
+      
+      jV.stderr.on('data', function (data) {
+        console.log('stderr: ' + data.toString());
+      });
+      
+      jV.on('exit', function (code) {
+        console.log('child process exited with code ' + code.toString());
+      });
     res.send("<h1>Hi</h1>")
 })
 
